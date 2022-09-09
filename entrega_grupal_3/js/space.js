dfaeed5e50f5d060ac0d7d4171e8bee7edc35252
evelyngrigorio <110796875+evelyngrigorio@users.noreply.github.com>
2022-09-08 22:37:58 -0300

    Initial commit

---
+++ entrega_grupal_3/js/space.js
@@ -0,0 +1,68 @@
+let busqueda = document.getElementById("inputBuscar");
+let contenedor = document.getElementById("contenedor");
+let btn = document.getElementById("btnBuscar");
+
+let getJSONData = function(URL){
+    let result = {};
+    return fetch(URL)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        return result;
+    });
+}
+
+
+btn.addEventListener("click", (e)=>{
+    e.preventDefault(); 
+    let items = [];
+
+    let URL = `https://images-api.nasa.gov/search?q=${busqueda.value}`;
+    getJSONData(URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            items = resultObj.data.collection.items
+            
+            let contenido = "";
+
+            items.forEach((item)=>{
+                
+                contenido += htmlContentToAppend(item.links, item.data);
+            })
+            contenedor.innerHTML = contenido;
+        } else {
+            contenedor.innerHTML = "";
+        }
+    });
+
+})
+
+function htmlContentToAppend(links, data) {
+    return `
+    <div class= "col-4" id="contenedor">
+    <div class="list-group-item list-group-item-action">
+                <div class="col-12">
+                 <img src="${links ? links[0].href: ''}" alt="" class="img-thumbnail"> 
+                 </div>
+                 <div >
+                    <div class="mb-1 text-break">
+                        <h4>${data[0].title}</h4>
+                        <p class="description overflow-auto"> ${data[0].description} </p>
+                        <small class="text-muted"> ${data[0].date_created} </small>    
+                    </div>
+                </div>
+    </div>
+    </div>
+    `} /*${links ? links[0].href: ''} valida si links está definido o no, 
+    si existe links devolve link[0].href, sino devolvé un string vacío */
