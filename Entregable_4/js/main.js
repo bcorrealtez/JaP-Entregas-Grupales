823494da9a20ecfca663703b54807348427526bb
BrunCoA <brunocorreaaltez2016@gmail.com>
2022-10-02 13:33:20 -0300

    pagina final

--- Entregable_4/js/main.js
+++ Entregable_4/js/main.js
@@ -22,38 +22,17 @@ japflix.render();
 btn.addEventListener("click", (e)=>{
     e.preventDefault();
 
-    let mostrarTitle = buscarTitle();
-    let mostrarTagline = buscarTagline();
-    let mostrarOverview = buscarOverview();
-    let mostrarGenres = buscarGenres();
-
-    let seriesMostrar = mostrarTitle.concat(mostrarTagline, mostrarOverview, mostrarGenres);
+    let seriesMostrar = seriesLista.filter(elem => 
+        elem.title.toLowerCase().includes(busqueda.value.toLowerCase())|| 
+        elem.tagline.toLowerCase().includes(busqueda.value.toLowerCase())||
+        elem.overview.toLowerCase().includes(busqueda.value.toLowerCase())||
+        elem.genres.some(elemento => ((elemento.name.toLowerCase().includes(busqueda.value.toLowerCase()))))
+        );
 
     console.log(seriesMostrar);
 
     mostrarSeries(seriesMostrar);
 });
-
-function buscarTitle(){
-    let mostrarTitle = seriesLista.filter(elemento => elemento.title.toLowerCase().includes(busqueda.value.toLowerCase()));
-    return mostrarTitle;
-}
-
-function buscarTagline(){
-    let mostrarTagline = seriesLista.filter(elemento => elemento.tagline.toLowerCase().includes(busqueda.value.toLowerCase()));
-    return mostrarTagline;
-}
-
-function buscarOverview(){
-    let mostrarOverview = seriesLista.filter(elemento => elemento.overview.toLowerCase().includes(busqueda.value.toLowerCase()));
-    return mostrarOverview;
-}
-
-function buscarGenres(){
-    let mostrarGenres = seriesLista.filter(elemento => elemento.genres.some(elemento => 
-        ((elemento.name.toLowerCase().includes(busqueda.value.toLowerCase())))));
-    return mostrarGenres;
-}
 
 function mostrarSeries(seriesMostrar){
 
@@ -65,7 +44,7 @@ function mostrarSeries(seriesMostrar){
 
         htmlcontenttoAppend += `
         
-        <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-start bg-dark p-2 m-2 border-muted border-bottom" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop" onclick="infoSerie(${seriesMostrar[i].id})">
+        <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-start bg-dark p-2 m-0" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop" onclick="infoSerie(${seriesMostrar[i].id})">
             <div class="ms-2 me-auto text-white ">
                 <div class="fw-bold">${seriesMostrar[i].title}</div>
                 <span class="text-muted">${seriesMostrar[i].tagline}</span>
@@ -107,20 +86,23 @@ function infoSerie(seriesMostrar){
             <h5 class="offcanvas-title" id="offcanvasTopLabel">${serie[0].title}</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-        <div class="offcanvas-body overflow-show">
+        <div class="offcanvas-body overflow-visible">
             <p>${serie[0].overview}</p>
-            <span class="text-muted">${generos.join(" - ")}</span>
-        </div>
-        <div class="dropdown d-flex justify-content-end">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                More info
-            </button>
-            <ul class="dropdown-menu p-2">
-                <li>Year: ${fecha.getFullYear()}</li>
-                <li>Runtime:  ${serie[0].runtime} mins.</li>
-                <li>Budget: $ ${serie[0].budget}</li>
-                <li>Revenue: $ ${serie[0].revenue}</li>
-            </ul>
+            <hr>
+            <div class="row overflow-visible">
+                <div class="dropdown d-flex col justify-content-between overflow-visible">
+                    <p class="text-muted my-auto">${generos.join(" - ")}</p>  
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        More info
+                    </button>
+                    <ul class="dropdown-menu p-2">
+                        <li>Year: ${fecha.getFullYear()}</li>
+                        <li>Runtime:  ${serie[0].runtime} mins.</li>
+                        <li>Budget: $ ${serie[0].budget}</li>
+                        <li>Revenue: $ ${serie[0].revenue}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
         
     `;
