4fb92ec13c19e3225cbc9597b96a9a6cabb63568
yandiralopez <110851421+yandiralopez@users.noreply.github.com>
2022-10-27 18:24:39 -0300

    Add files via upload
    falta texto que pide validar checkbox

---
+++ Entregable_6/js/validacion.js
@@ -0,0 +1,37 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            validarContrasenas()
+            event.preventDefault()
+            event.stopPropagation()
+          
+          }
+          
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+  const pass1 = document.getElementById("password1");
+  const pass2 = document.getElementById("password2");
+
+  function validarContrasenas(){
+    if(pass2.value === pass1.value && pass1.checkValidity()){
+      pass2.setCustomValidity("");
+    } 
+    else {
+      pass2.setCustomValidity('Debe ser igual a "contraseña"');
+    }
+    pass2.reportValidity();
+  }
+
+  pass2.addEventListener("input", validarContrasenas);
