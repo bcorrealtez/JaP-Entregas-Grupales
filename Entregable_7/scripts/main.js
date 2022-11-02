e742c3b6d29d3e3cfcf338ae7507b60697407eb8
bcorrealtez <brunocorreaaltez2016@gmail.com>
2022-11-02 17:56:55 -0300

    agregar y borrar a medio haacer

--- Entregable_7/scripts/main.js
+++ Entregable_7/scripts/main.js
@@ -7,37 +7,76 @@ const modificar = document.getElementById("btnPut");
 const borrar = document.getElementById("btnDelete");
 const nomPost = document.getElementById("inputPostNombre");
 const apePost = document.getElementById("inputPostApellido");
+const inpDel = document.getElementById("inputDelete");
 
 document.getElementById("post-box").addEventListener("input", () => {
   if (nomPost.value && apePost.value) {
-    
     agregar.removeAttribute("disabled")
   } else {
     agregar.setAttribute("disabled", "")
   }
 });
 
-buscar.addEventListener("click", () => {});
-
-agregar.addEventListener("click", () => {
-    data.name = nomPost.value;
-    data.lastname = apePost.value;
-    peticion()
+document.getElementById("delete-box").addEventListener("input", () => {
+  if (inpDel.value) {
+    borrar.removeAttribute("disabled")
+  } else {
+    borrar.setAttribute("disabled", "")
+  }
 });
 
-modificar.addEventListener("click", () => {});
+buscar.addEventListener("click", () => { });
 
-borrar.addEventListener("click", () => {});
+agregar.addEventListener("click", () => {
+  data.name = nomPost.value;
+  data.lastname = apePost.value;
+  peticion("POST")
+});
+
+modificar.addEventListener("click", () => { });
+
+borrar.addEventListener("click", () => { });
 
 function peticion(method) {
-  fetch(url, {
-    method: method,
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .catch((error) => console.error("Error:", error))
-    .then((response) => console.log("Success:", response));
+  switch (method) {
+    case "GET":
+      fetch(url, {
+        method: method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .catch((error) => console.error("Error:", error))
+        .then((response) => console.log("Success:", response));
+
+      break;
+
+    case "POST":
+      fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .catch((error) => console.error("Error:", error))
+        .then((response) => console.log("Success:", response));
+
+      break;
+
+    case "PUT":
+
+      break;
+
+    case "DELETE":
+
+      break;
+
+    default:
+      break;
+  }
+
+
 }
