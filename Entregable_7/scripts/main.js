75f7eb20cf422393b1d4a1dc1af8d0b292afd223
BrunCoA <brunocorreaaltez2016@gmail.com>
2022-11-03 18:11:21 -0300

    entregable 7, agregar y borrar terminados

--- Entregable_7/scripts/main.js
+++ Entregable_7/scripts/main.js
@@ -1,70 +1,86 @@
-/*const url = "https://6361aa6167d3b7a0a6cad6ee.mockapi.io/users/"; //Bruno */
-const url = "https://6361a80267d3b7a0a6cab68d.mockapi.io/users/"; //paola
+const url = "https://6361aa6167d3b7a0a6cad6ee.mockapi.io/users/"; //Bruno
+//const url = "https://6361a80267d3b7a0a6cab68d.mockapi.io/users/"; paola
 const data = { name: "", lastname: "" };
 
 const buscar = document.getElementById("btnGet1");
 const agregar = document.getElementById("btnPost");
 const modificar = document.getElementById("btnPut");
 const borrar = document.getElementById("btnDelete");
+const results = document.getElementById("results");
 const nomPost = document.getElementById("inputPostNombre");
 const apePost = document.getElementById("inputPostApellido");
-<<<<<<< HEAD
 const inpDel = document.getElementById("inputDelete");
-=======
-const results = document.getElementById("results");
->>>>>>> 691cad32e3751812d4bf393ce2d54dc4688d771a
+const inpGet = document.getElementById("inputGet1Id");
+
+// INPUT Event
 
 document.getElementById("post-box").addEventListener("input", () => {
+  /^\s/.test(nomPost.value) ? (nomPost.value = "") : "";
+  /^\s/.test(apePost.value) ? (apePost.value = "") : "";
+
   if (nomPost.value && apePost.value) {
-    agregar.removeAttribute("disabled")
+    agregar.removeAttribute("disabled");
   } else {
-    agregar.setAttribute("disabled", "")
+    agregar.setAttribute("disabled", "");
   }
 });
 
-document.getElementById("delete-box").addEventListener("input", () => {
+inpDel.addEventListener("input", () => {
   if (inpDel.value) {
-    borrar.removeAttribute("disabled")
+    borrar.removeAttribute("disabled");
   } else {
-    borrar.setAttribute("disabled", "")
+    borrar.setAttribute("disabled", "");
   }
 });
 
-buscar.addEventListener("click", () => { });
+// CLICK Event
+
+buscar.addEventListener("click", () => {
+  if (inpGet.value) {
+    peticion("GETid");
+  } else {
+    peticion("GET");
+  }
+});
 
 agregar.addEventListener("click", () => {
-<<<<<<< HEAD
   data.name = nomPost.value;
   data.lastname = apePost.value;
-  peticion("POST")
+  peticion("POST");
 });
 
-modificar.addEventListener("click", () => { });
-
-borrar.addEventListener("click", () => { });
-=======
-    data.name = nomPost.value;
-    data.lastname = apePost.value;
-    peticion("POST");
-    
-    htmlContentToApend = mostrar;
-    results.innerHTML = htmlContentToApend;
-});
-
-modificar.addEventListener("click", () => {
-
-});
+modificar.addEventListener("click", () => {});
 
 borrar.addEventListener("click", () => {
-
+  peticion("DELETE");
 });
->>>>>>> 691cad32e3751812d4bf393ce2d54dc4688d771a
+
+modificar.addEventListener("click", () => {});
+
+borrar.addEventListener("click", () => {});
+
+// RESULTADOS
+
+// FETCH
 
 function peticion(method) {
   switch (method) {
     case "GET":
       fetch(url, {
-        method: method,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .catch((error) => console.error("Error:", error))
+        .then((response) => console.log("Success:", response));
+
+      break;
+
+    case "GETid":
+      fetch(url+inpGet.value, {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
@@ -77,7 +93,7 @@ function peticion(method) {
 
     case "POST":
       fetch(url, {
-        method: method,
+        method: "POST",
         body: JSON.stringify(data),
         headers: {
           "Content-Type": "application/json",
@@ -90,16 +106,23 @@ function peticion(method) {
       break;
 
     case "PUT":
-
       break;
 
     case "DELETE":
+      fetch(url + inpDel.value, {
+        method: "DELETE",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .catch((error) => console.error("Error:", error))
+        .then((response) => console.log("Success:", response));
 
       break;
 
     default:
       break;
   }
-
-
 }
